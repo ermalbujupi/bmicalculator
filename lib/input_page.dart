1afe8c6c7ce9f307d5f6893cf34b7b76fa178463
Ermal Bujupaj <ermal.bj@gmail.com>
2021-02-01 22:16:07 +0100

    Add customized round button.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -147,3 +147,22 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
+
+class RoundIconButton extends StatelessWidget {
+  RoundIconButton({this.icon, this.onPressed});
+
+  final IconData icon;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      onPressed: onPressed,
+      shape: CircleBorder(),
+      disabledElevation: 6.0,
+      fillColor: Color(0xFF4C4F5E),
+      constraints: BoxConstraints.tightFor(width: 56.0, height: 56.0),
+    );
+  }
+}
